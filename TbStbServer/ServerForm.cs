7523c46690685cd5d9dbf95d5496e3c82daeac76
Arne (USA) <aleitert@kent.edu>
2019-11-24 03:01:53 -0800

    Added 'empty' project for server.

---
+++ TbStbServer/ServerForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TbStb.Server
+{
+    public partial class ServerForm : Form
+    {
+        public ServerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
