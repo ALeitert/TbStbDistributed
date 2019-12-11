6c3722a5b4a85527dafd0501aeb9b0ffe691ca50
Arne (USA) <aleitert@kent.edu>
2019-12-10 19:26:51 -0800

    Updated graph implementation (mostly formating).

--- PotentialPartners/Graph.cpp
+++ PotentialPartners/Graph.cpp
@@ -3,143 +3,146 @@
 //
 
 #include "Graph.h"
-#include <unordered_set>
-#include <vector>
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
 
 using namespace std;
 using h_set = std::unordered_set<int>;
 
-//create default constructor
-Graph::Graph(){
+// Default constructor.
+Graph::Graph()
+{
     noOfVertices = 0;
-    capacity = 0;
+    edges = nullptr;
 }
 
-//constructor read from file
-Graph::Graph (ifstream & infile) {
-    int neighbors = 0, v = 0;
+// Copy constructor.
+Graph::Graph(const Graph& g)
+{
+    this->noOfVertices = g.noOfVertices;
 
+    edges = new int*[noOfVertices];
 
-    infile >> v;
-    noOfVertices = v;
-    capacity = v;
-    incomingEdges = new h_set*[noOfVertices];
-    outgoingEdges = new h_set*[noOfVertices];
+    for (int i = 0; i < noOfVertices; i++)
+    {
+        int deg = g.edges[i][0];
 
-    for(int i = 0; i < noOfVertices; i++){
-        incomingEdges[i] = new h_set;
-        outgoingEdges[i] = new h_set;
-        infile >> neighbors;
+        edges[i] = new int[deg + 1];
+        edges[i][0] = deg;
 
-        for(int j = 0; j < neighbors; j++){
-            infile >> v;
-            incomingEdges[i]->insert(v);
-            outgoingEdges[i]->insert(v);
-        }
-    }
-    infile.close();
-}
-
-//copy constructor
-Graph::Graph (const Graph & orig){
-    this->noOfVertices = orig.noOfVertices;
-    this->capacity = orig.capacity;
-
-    incomingEdges = new h_set*[noOfVertices];
-    outgoingEdges = new h_set*[noOfVertices];
-
-    for(int i = 0; i < noOfVertices; i++){
-        incomingEdges[i] = new h_set;
-        outgoingEdges[i] = new h_set;
-
-        for(const int vert: *orig.incomingEdges[i]){
-            incomingEdges[i]->insert(vert);
-            outgoingEdges[i]->insert(vert);
+        for (int j = 1; j <= deg; j++)
+        {
+            edges[i][j] = g.edges[i][j];
         }
     }
 }
 
-vector<int>* Graph::bfs(int startId){
+// Constructor read from file.
+Graph::Graph(istream& in)
+{
+    in >> noOfVertices;
+
+    edges = new int*[noOfVertices];
+
+    for (int i = 0; i < noOfVertices; i++)
+    {
+        int deg = 0;
+        in >> deg;
+
+        edges[i] = new int[deg + 1];
+        edges[i][0] = deg;
+
+        for (int j = 1; j <= deg; j++)
+        {
+            int nId;
+            in >> nId;
+            edges[i][j] = nId;
+        }
+    }
+}
+
+int Graph::getVerts()
+{
+    return this->noOfVertices;
+}
+
+vector<int>* Graph::bfs(int startId)
+{
     return limitedBFS(startId, INT_MAX);
 }
 
-////currently set up for all consecutive vertices
 vector<int>* Graph::limitedBFS(int startId, int maxDis)
 {
     vector<int> distances = vector<int>();
     vector<int> q = vector<int>();
     h_set visited;
 
-    // Set start vertex
+    // Start vertex.
     distances.push_back(0);
     q.push_back(startId);
     visited.insert(startId);
 
     for (int qInd = 0; qInd < q.size() && distances[qInd] < maxDis; qInd++)
     {
-        int vInd = q[qInd];
+        int vId = q[qInd];
+        int vDeg = edges[vId][0];
+
         int nDis = distances[qInd] + 1;
 
-        for (const int uInd: *outgoingEdges[vInd])
+        for (int j = 1; j <= vDeg; j++)
         {
-            if (visited.count(uInd) == 0)
-            {
-                distances.push_back(nDis);
-                q.push_back(uInd);
-                visited.insert(uInd);
-            }
+            int uId = edges[vId][j];
+
+            if (visited.count(uId) > 0) continue;
+
+            distances.push_back(nDis);
+            q.push_back(uId);
+            visited.insert(uId);
         }
     }
 
     vector<int>* bfsResult = new vector<int>[2];
-    bfsResult[0] = q;
-    bfsResult[1] = distances;
+    bfsResult[0] = move(q);
+    bfsResult[1] = move(distances);
+
     return bfsResult;
 }
 
-
-///traverse up partition, run bfs from any point that = -1, assign int value to designate which partition, must not run
-/// over any point that does not equal -1 in partition;  Future- hash table that assigns points to parition? Will vertices
-/// ever be non-sequential?
-int Graph::findConnComp(int *partition, int pLength){
-    //tracks which partition a point belongs in
+// Traverse up partition, run bfs from any point that = -1, assign int value to designate which partition,
+// must not run over any point that does not equal -1 in partition;
+// Future- hash table that assigns points to parition?
+// Will vertices ever be non-sequential?
+int Graph::findConnComp(int* partition, int pLength)
+{
+    // Tracks which partition a point belongs to.
     int pNum = 1;
 
-    //check if each point has been assigned
-    for (int p = 0; p < pLength; p++) {
+    // Check if each point has been assigned.
+    for (int p = 0; p < pLength; p++)
+    {
+        if (partition[p] != -1) continue;
 
-        if (partition[p] == - 1) {
-            partition[p] = pNum;
+        partition[p] = pNum;
 
-            vector<int> q;
+        vector<int> q;
+        q.push_back(p);
 
-            // Set start vertex
-            q.push_back(p);
+        for (int qInd = 0; qInd < q.size(); qInd++)
+        {
+            int vId = q[qInd];
+            int vDeg = edges[vId][0];
 
-            for (int qInd = 0; qInd < q.size(); qInd++)
+            for (int j = 1; j <= vDeg; j++)
             {
-                int vInd = q[qInd];
+                int uId = edges[vId][j];
 
-                for (const int uInd: *outgoingEdges[vInd])
-                {
-                    if (partition[uInd] != -1)
-                    {
-                        continue;
-                    }
-                    q.push_back(uInd);
-                    partition[uInd] = pNum;
-                }
+                if (partition[uId] != -1) continue;
+
+                q.push_back(uId);
+                partition[uId] = pNum;
             }
-            pNum++;
         }
+
+        pNum++;
     }
+
     return pNum;
-}
-
-
-int Graph::getVerts(){
-    return this->noOfVertices;
 }
--- PotentialPartners/Graph.h
+++ PotentialPartners/Graph.h
@@ -1,30 +1,33 @@
 //
 // Created by Holly Strauch on 10/1/2019.
 //
+
+#include <istream>
 #include <unordered_set>
-#include <fstream>
 #include <vector>
+
 using namespace std;
+
 #ifndef STBRESEARCH_GRAPH_H
 #define STBRESEARCH_GRAPH_H
 
-
-
-class Graph {
-    int noOfVertices;
-    int capacity;
-    std::unordered_set<int>** incomingEdges;
-    std::unordered_set<int>** outgoingEdges;
+class Graph
+{
 public:
     Graph();
-    explicit Graph(ifstream & infile);
-    Graph (const Graph & orig);
-    vector<int>* limitedBFS(int startId, int maxDis);
-    vector<int>* bfs(int startId);
-    int getVerts();
-    int findConnComp(int *partition, int pLength);
-    //bfs
+    Graph(const Graph& g);
+    explicit Graph(istream& in);
 
+    int getVerts();
+
+    vector<int>* bfs(int startId);
+    vector<int>* limitedBFS(int startId, int maxDis);
+
+    int findConnComp(int *partition, int pLength);
+
+private:
+    int noOfVertices;
+    int** edges;
 };
 
-#endif //STBRESEARCH_GRAPH_H
+#endif
