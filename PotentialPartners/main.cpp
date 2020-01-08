3e57b9a3e15c0209ebfe8dfa43542c7f09062e2f
Arne (USA) <aleitert@kent.edu>
2020-01-07 18:56:18 -0800

    Added code for main().

--- PotentialPartners/main.cpp
+++ PotentialPartners/main.cpp
@@ -1,7 +1,34 @@
-#include<iostream>
+#include <iostream>
+
+#include "Graph.h"
+#include "PotPart.h"
+#include "STBAlg.h"
 
 int main()
 {
+    Graph g(cin);
+
+    STBAlg algo;
+
+    while (true)
+    {
+        int uId = -1;
+        int rho = -1;
+
+        cin >> uId;
+        cin >> rho;
+
+        if (uId < 0 || uId >= g.getVerts() || rho <= 0)
+        {
+            break;
+        }
+
+        PotPart* potPar = algo.findPotPart(g, uId, rho);
+        potPar->print(cout);
+
+        delete potPar;
+    }
+
 #ifdef _DEBUG
     system("pause");
 #endif
