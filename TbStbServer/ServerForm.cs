bc2a0729432f9145dc6780962517df19872fdf6e
Arne (USA) <aleitert@kent.edu>
2020-01-11 10:43:53 -0800

    Added alternating back colors and activated double buffering for lists.

--- TbStbServer/ServerForm.cs
+++ TbStbServer/ServerForm.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Drawing;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace TbStb.Server
 {
     public partial class ServerForm : Form
     {
+        private Color DefaultItemBackColor = Color.White;
+        private Color ShadedItemBackColor = Color.FromArgb(228, 232, 237);
+
         private bool logActive = true;
         private delegate void LogDelegate(string text);
 
         public ServerForm()
         {
             InitializeComponent();
+
+            PropertyInfo pi = typeof(ListView).GetProperty("DoubleBuffered", (BindingFlags)(-1));
+            pi.SetValue(ltvGraphs, true, new object[] { });
+            pi.SetValue(ltvClients, true, new object[] { });
+            pi.SetValue(ltvLog, true, new object[] { });
         }
 
         private void Log(string text)
@@ -26,6 +36,8 @@ namespace TbStb.Server
                 ListViewItem lvi = new ListViewItem();
                 lvi.Text = DateTime.Now.ToString("HH:mm:ss");
                 lvi.SubItems.Add(text);
+
+                lvi.BackColor = ltvLog.Items.Count % 2 == 0 ? DefaultItemBackColor : ShadedItemBackColor;
 
                 ltvLog.Items.Add(lvi);
             }
